64d04fa4d8228dce45b206888f6802703a6fa635
Dany <dany21sayan_@hotmail.com>
2023-03-15 04:01:55 +0000

    food CRUD operations

--- menu/forms.py
+++ menu/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from .models import Category
+from .models import Category, FoodItem
+from accounts.validators import allow_only_images_validator
 
 
 
@@ -8,3 +9,10 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['category_name', 'description']
+
+
+class FoodItemForm(forms.ModelForm):
+    image = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info 2-100'}), validators=[allow_only_images_validator])
+    class Meta:
+        model = FoodItem
+        fields = ['category', 'food_title', 'description', 'price', 'image', 'is_available']
